506dd3fec0d7178a4790760671e3588802ee2af4
Jan Puto <tofukles@gmail.com>
2024-12-18 01:26:53 +0100

    Window.h setup file, window class

---
+++ MXWindow.cpp
@@ -0,0 +1,11 @@
+#include "MXWindow.h"
+
+MXWindow::MXWindow( int const Width, int const Height, char const* Name )
+{	
+	
+}
+
+MXWindow::~MXWindow()
+{
+	DestroyWindow( hWnd );
+}
---
+++ MXWindow.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "MXWindowSetup.h"
+
+class MXWindow
+{
+public:
+	MXWindow( int const Width, int const Height, char const* Name );
+	~MXWindow();
+	MXWindow( MXWindow const& ) = delete; // no need for copy constructor
+
+protected:
+	int Width = 0;
+	int Height = 0;
+	HWND hWnd;
+};
